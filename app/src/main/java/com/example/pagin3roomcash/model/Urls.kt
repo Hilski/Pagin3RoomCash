df882965fc325fa4d30a53676e53fe76c9d71523
hilski@mail.ru <hs60ssbv>
2023-03-29 23:01:16 +0300

    Api ready

---
+++ app/src/main/java/com/example/pagin3roomcash/model/Urls.kt
@@ -0,0 +1,9 @@
+package com.example.pagin3roomcash.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Urls (
+    val regularImage: String
+)
---
+++ app/src/main/java/com/example/pagin3roomcash/model/UserLinks.kt
@@ -0,0 +1,8 @@
+package com.example.pagin3roomcash.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserLinks(
+    val html: String
+)
